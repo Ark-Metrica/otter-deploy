39f1b390c65d29384e12cf08bfbbfcbd1d43ca75
Grey Christoforo <grey@arkmetrica.com>
2020-07-08 09:30:57 +0100

    Update join-wg.sh

--- scripts/join-wg.sh
+++ scripts/join-wg.sh
@@ -6,12 +6,14 @@ then
   exit 1
 fi
 
+PORT=${1:-48872}
+
 pacman -S --needed --noconfirm python wireguard-tools curl 2>&1 >/dev/null
 curl -fsSL -o /bin/wg-request https://raw.githubusercontent.com/greyltc/wg-request/master/wg-request 2>&1 >/dev/null
 chmod +x /bin/wg-request 2>&1 >/dev/null
 
 wg genkey | tee /tmp/peer_A.key | wg pubkey > /tmp/peer_A.pub
-timeout 5 python3 /bin/wg-request --port 48872 --private-key $(cat /tmp/peer_A.key) $(cat /tmp/peer_A.pub) pipe.0x3.ca > /etc/wireguard/wg0.conf 2>/dev/null
+timeout 5 python3 /bin/wg-request --port "${PORT}" --private-key $(cat /tmp/peer_A.key) $(cat /tmp/peer_A.pub) pipe.0x3.ca > /etc/wireguard/wg0.conf 2>/dev/null
 wg-quick up wg0 2>&1 >/dev/null
 systemctl enable wg-quick@wg0 2>&1 >/dev/null
 
