30273d64f317f6b4d7a51d9b5e74ad18f09a30ee
Grey Christoforo <grey@christoforo.net>
2020-08-02 20:43:14 +0100

    Update provision1.sh

--- scripts/provision1.sh
+++ scripts/provision1.sh
@@ -2,13 +2,24 @@
 sudo reflector --latest 200 --protocol http --protocol https --sort rate --save /etc/pacman.d/mirrorlist
 sudo bash -c "yes | pacman -Syyuu --needed wireguard-tools"
 sudo sed -i 's|ChallengeResponseAuthentication no|ChallengeResponseAuthentication yes|g' /etc/ssh/sshd_config
+
 sudo curl -fsSL -o /bin/join-wg https://raw.githubusercontent.com/greyltc/wg-request/master/join-wg.sh
 sudo chmod +x /bin/join-wg
+
 sudo cp /vagrant/scripts/wg-helper /bin/.
 sudo chmod +x /bin/wg-helper
+
 sudo cp /vagrant/scripts/wg-helper.service /etc/systemd/system/.
 sudo chmod 644 /etc/systemd/system/wg-helper.service
 sudo systemctl enable wg-helper.service
+
+sudo cp /vagrant/scripts/vwait /bin/.
+sudo chmod +x /bin/vwait
+
+sudo cp /vagrant/scripts/vwait.service /etc/systemd/system/.
+sudo chmod 644 /etc/systemd/system/vwait.service
+sudo systemctl enable vwait.service
+
 sudo cp /etc/systemd/network/eth0.network /etc/systemd/network/eth1.network
 sudo sed -i 's|Name=eth0|Name=eth1|g' /etc/systemd/network/eth1.network
 sudo systemctl restart sshd
