bd9a7b9df532f9f3c11ba098ff58354e139dd3d4
Grey Christoforo <grey@arkmetrica.com>
2020-08-02 13:35:49 +0100

    Update provision1.sh

--- scripts/provision1.sh
+++ scripts/provision1.sh
@@ -4,11 +4,9 @@ sudo bash -c "yes | pacman -Syyuu --needed wireguard-tools"
 sudo sed -i 's|ChallengeResponseAuthentication no|ChallengeResponseAuthentication yes|g' /etc/ssh/sshd_config
 sudo curl -fsSL -o /bin/join-wg https://raw.githubusercontent.com/greyltc/wg-request/master/join-wg.sh
 sudo chmod +x /bin/join-wg
-sudo join-wg pipe.0x3.ca 48872
 sudo cp /etc/systemd/network/eth0.network /etc/systemd/network/eth1.network
 sudo sed -i 's|Name=eth0|Name=eth1|g' /etc/systemd/network/eth1.network
 sudo systemctl restart sshd
 sudo systemctl restart systemd-networkd
 sudo systemctl disable netctl@eth1.service
-#sudo bash -c "yes | pacman -Syu --needed virtualbox-guest-utils ansible git"
 sudo bash -c "yes | pacman -Syu --needed virtualbox-guest-utils git"
