bcced8d34771f673ebe00d4ce169b92a7e45daf4
Greyson Christoforo <grey@christoforo.net>
2020-08-03 11:46:02 +0100

    unattended provisioning & packaging should work

--- Vagrantfile
+++ Vagrantfile
@@ -38,7 +38,8 @@ Vagrant.configure("2") do |config|
   # Create a public network, which generally matched to bridged network.
   # Bridged networks make the machine appear as another physical device on
   # your network.
-  config.vm.network "public_network", auto_config: false
+  #config.vm.network "public_network", auto_config: false
+  config.vm.network "public_network", auto_config: false, bridge: "eno1" # for bridge interface auto-select
 
   # Share an additional folder to the guest VM. The first argument is
   # the path on the host to the actual folder. The second argument is
--- build_box.sh
+++ build_box.sh
@@ -1,8 +1,29 @@
 #!/usr/bin/env bash
 
+MACHINE="measurebox_dev"
+
+# provision
 vagrant up
-vagrant halt
+
+# dump ssh config
+vagrant ssh-config > vagrant-ssh
+
+# ask machine to shutdown with password login
+sshpass -p vagrant ssh -F vagrant-ssh vagrant:@default 'sudo shutdown -P now' # shutdown with password login
+rm vagrant-ssh
+
+# wait for machine to shutdown
+until $(VBoxManage showvminfo --machinereadable ${MACHINE} | grep -q ^VMState=.poweroff.)
+do
+  sleep 1
+done
+sleep 1
+
+# package the box
 rm out.box
 rm -rf vtmp
-VAGRANT_HOME=./vtmp vagrant package --base measurebox_dev --output ./out.box
+VAGRANT_HOME=./vtmp vagrant package --base ${MACHINE} --output ./out.box
+rm -rf vtmp
+
+# read its checksum
 sha512sum out.box
