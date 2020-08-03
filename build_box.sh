c6979078767f988ca6b60c3ebb1cd411776d0167
Greyson Christoforo <grey@christoforo.net>
2020-08-03 12:42:38 +0100

    exit on error

--- build_box.sh
+++ build_box.sh
@@ -1,18 +1,25 @@
 #!/usr/bin/env bash
+set -e
 
 MACHINE="measurebox_dev"
 
 # provision
 vagrant up
 
+# replace the ssh key and shut off the machine
+vagrant ssh -c 'curl -fsSL -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub; chmod 700 ~/.ssh; chmod 600 ~/.ssh/authorized_keys; chown -R vagrant:vagrant ~/.ssh; history -r; sudo shutdown -P now'
+
 # dump ssh config
-vagrant ssh-config > vagrant-ssh
+#vagrant ssh-config > vagrant-ssh
+
+# replace the ssh key and shut off the machine
+#sshpass -p vagrant ssh -F vagrant-ssh vagrant:@default 'curl -fsSL -o /home/vagrant/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub;chmod 700 ~/.ssh;chmod 600 ~/.ssh/authorized_keys;chown -R vagrant:vagrant ~/.ssh'
 
 # ask machine to shutdown with password login
-sshpass -p vagrant ssh -F vagrant-ssh vagrant:@default 'sudo shutdown -P now' # shutdown with password login
-rm vagrant-ssh
+#sshpass -p vagrant ssh -F vagrant-ssh vagrant:@default 'sudo shutdown -P now'
+#rm vagrant-ssh
 
-# wait for machine to shutdown
+# wait for the shutdown to complete
 until $(VBoxManage showvminfo --machinereadable ${MACHINE} | grep -q ^VMState=.poweroff.)
 do
   sleep 1
